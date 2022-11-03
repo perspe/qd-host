1ab2bc075174166413e458f0a3be04bd55cfe19d
perspe <m.alexandre@campus.fct.unl.pt>
2022-11-03 17:09:31 +0000

    setup.py builds everything

--- band_model/setup.py
+++ band_model/setup.py
@@ -4,12 +4,16 @@ Basic setup to compile with distutils to create c file
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import os
+
+PY_SMM = os.path.join("smm", "py_smm_base.pyx")
+CPP_SMM = os.path.join("smm", "smm_base.cpp")
+ZEROS_CALC = os.path.join("cython_utils", "zeros_calc.pyx")
 
 extensions = [
-    Extension("cython_utils.zeros_calc",
-              ["cython_utils/zeros_calc.pyx"])
+    Extension("smm.py_smm_base", [PY_SMM, CPP_SMM], include_dirs=["smm"]),
+    Extension("cython_utils.zeros_calc", [ZEROS_CALC], include_dirs=[numpy.get_include()])
 ]
 extensions = cythonize(extensions)
 
-setup(ext_modules=extensions,
-      include_dirs=[numpy.get_include()])
+setup(ext_modules=extensions)
